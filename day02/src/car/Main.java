c34c0ce77c91fd3b6853b9dd9afcf0d50c667187
jmlee <jmlee@tonesol.com>
2025-07-04 14:25:42 +0900

    0704

---
+++ day02/src/car/Main.java
@@ -0,0 +1,20 @@
+package car;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Start Application...");
+
+        Car car = new Car();
+        car.go();
+        car.stop();
+
+        Car car1 = new Car("k1","red",1000);
+        car1.go();
+        car1.stop();
+
+
+        System.out.println("End Application...");
+    }
+}
