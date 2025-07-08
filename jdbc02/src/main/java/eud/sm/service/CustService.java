a189dabbf94e32406442fcd681a2e812c63d2074
jmlee <jmlee@tonesol.com>
2025-07-08 14:23:52 +0900

    0708

--- jdbc02/src/main/java/eud/sm/service/CustService.java
+++ jdbc02/src/main/java/eud/sm/service/CustService.java
@@ -1,22 +1,40 @@
 package eud.sm.service;
 
 import eud.sm.dto.Cust;
+import eud.sm.frame.ConnectionPool;
 import eud.sm.frame.SmService;
 import eud.sm.repository.CustRepository;
 
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.List;
 
 public class CustService implements SmService<Cust, String> {
 
     CustRepository custRepository;
+    ConnectionPool connectionPool;
 
     public CustService() {
         this.custRepository = new CustRepository();
+        try {
+            connectionPool = ConnectionPool.create();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
     public void register(Cust cust) throws Exception {
-
+        Connection conn = connectionPool.getConnection();
+        try{
+            custRepository.insert(cust, conn);
+        }catch(Exception e){
+            throw e;
+        }finally {
+            if (conn != null) {
+                connectionPool.releaseConnection(conn);
+            }
+        }
     }
 
     @Override
---
+++ jdbc02/src/test/java/cust/Insert.java
@@ -0,0 +1,25 @@
+package cust;
+
+import eud.sm.dto.Cust;
+import eud.sm.service.CustService;
+import org.junit.jupiter.api.Test;
+
+public class Insert {
+
+    @Test
+    public void insert(){
+        System.out.println("insert Test Start ...");
+        CustService custService = new CustService();
+        Cust cust = Cust.builder()
+                .custId("id01")
+                .custPwd("pwd55")
+                .custName("홍말순")
+                .build();
+        try {
+            custService.register(cust);
+            System.out.println("등록 정상");
+        } catch (Exception e) {
+            System.out.println("등록 오류");
+        }
+    }
+}
